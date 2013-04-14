e8dcf2f300de854e8f994dbaf8a6c5166850e2fa
Mike Lang <mikelang3000@gmail.com>
2013-04-15 03:38:14 +1000

    Some stuff for debugging that for stupid reasons I can't fully include here

--- debug.py
+++ debug.py
@@ -1,12 +1,18 @@
 import signal
-from gevent import sleep
+from gevent import sleep, greenlet
+from greenlet_tb import print_greenlets, extract_frames
+import traceback
 
-
-def starve_test():
+def starve_test(report=None):
 	"""Keep a SIGALRM perpetually pending...as long as we get rescheduled in a timely manner."""
 	def boom(sig, frame):
+		if report:
+			print_greenlets(report)
+			report.write('CURRENT:\n')
+			report.write(''.join(traceback.format_list(extract_frames(frame))) + '\n')
 		raise Exception("Greenlets being starved")
 	signal.signal(signal.SIGALRM, boom)
 	while 1:
 		signal.alarm(2)
 		sleep(0.1)
+
