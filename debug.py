bee75943fece5883696634d54d3510ff18b34f23
Mike Lang <mike@freelancer.com>
2013-05-03 16:28:54 +1000

    Revert "Some stuff for debugging that for stupid reasons I can't fully include here"
    This reverts commit e8dcf2f300de854e8f994dbaf8a6c5166850e2fa.

--- debug.py
+++ debug.py
@@ -1,18 +1,12 @@
 import signal
-from gevent import sleep, greenlet
-from greenlet_tb import print_greenlets, extract_frames
-import traceback
+from gevent import sleep
 
-def starve_test(report=None):
+
+def starve_test():
 	"""Keep a SIGALRM perpetually pending...as long as we get rescheduled in a timely manner."""
 	def boom(sig, frame):
-		if report:
-			print_greenlets(report)
-			report.write('CURRENT:\n')
-			report.write(''.join(traceback.format_list(extract_frames(frame))) + '\n')
 		raise Exception("Greenlets being starved")
 	signal.signal(signal.SIGALRM, boom)
 	while 1:
 		signal.alarm(2)
 		sleep(0.1)
-
