6817e959a7b79219299aa533cae96ab6d92e2f11
Mike Lang <mikelang3000@gmail.com>
2014-11-29 03:28:51 -0800

    awp.missing: verbose option

--- awp/missing.py
+++ awp/missing.py
@@ -6,20 +6,26 @@ Also takes optional --options as follows:
 	--nomagic: Do not use libmagic to identify audio files
 	--extensions: Space-seperated list of file extensions to recognise
 	--norecurse: Do not search subdirs
+	--verbose
 """
 
 from scriptlib import with_argv
 import awp.playlist
 
 @with_argv
-def main(filename, searchpath, nomagic=False, extensions=None, norecurse=False):
+def main(filename, searchpath, nomagic=False, extensions=None, norecurse=False, verbose=False):
 	if extensions: extensions = extensions.split()
 	playlist = awp.playlist.Playlist(filename)
 	found_list = awp.playlist.from_directory(searchpath, extensions=extensions, use_magic=(not nomagic), recurse=(not norecurse))
 	diff = playlist.diff(found_list)
+	if verbose:
+		print "{} contains {} entries".format(filename, len(playlist.entries))
+		print "{}/ contains {} entries".format(searchpath.rstrip('/'), len(found_list.entries))
 	for path, (us, them) in diff.items():
 		if us is None:
 			print path
+		if verbose and them is None:
+			print "WARNING: {} not in found list".format(path)
 
 if __name__=='__main__':
 	main()
